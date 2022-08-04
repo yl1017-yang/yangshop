294ef1cd6c3c782ab98bbfde11ee869269139ee0
yanglae <yanglae@hanmail.net>
2022-08-04 17:18:12 +0900

    @경로 추가

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir:'./docs',
   publicPath:'/yangshop',
+
+  //@ 경로설정 https://imkh.dev/vue-alias-path/
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@node_modules": path.resolve(__dirname, "./node_modules"),
+        "@src": path.resolve(__dirname, "./src"),
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@configs": path.resolve(__dirname, "./src/configs"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+      }
+    }
+  },
 })
