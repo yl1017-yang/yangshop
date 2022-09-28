554c6aa68ff20efc16d1aa767b2cae297910ba92
yanglae <yanglae@hanmail.net>
2022-09-28 16:37:34 +0900

    axios

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import mixins from "./mixins/mixins";
+import mixins from './mixins/mixins'
 import store from './store/store'
+import axios from 'axios'
+// axios.defaults.baseURL = 'http://localhost:3000'
+
+// Vue.prototype.$http = axios
+// Vue.config.productionTip = false
 
 //createApp(App).mount('#app')
 const app = createApp(App);
 app.use(router);
 app.mixin(mixins);
 app.use(store);
+app.config.globalProperties.$axios = axios; 
 app.mount("#app");
 
